f570cbd04e32d41fb9edab76ed908d79ae5940f9
ghaoyuxin <ghaoyuxin@gmail.com>
2020-02-03 08:27:57 -0500

    added AI script under monobehavior

---
+++ W1_Gao_Soccer/Assets/Script/AIScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIScript : MonoBehaviour
+{
+    private Vector2 ball;
+    private Rigidbody2D rb;
+    private void Start()
+    {
+        ball = GameObject.FindWithTag("Ball").transform.position;
+        rb = GetComponent<Rigidbody2D>();
+        print(ball);
+    }
+
+    private void Update()
+    {
+        rb.velocity = new Vector2(ball.x, ball.y);
+    }
+}
--- W1_Gao_Soccer/Assets/Script/PlayerController.cs
+++ W1_Gao_Soccer/Assets/Script/PlayerController.cs
@@ -6,7 +6,6 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float speed = 20;
-    public int score;
     public int playerNum = 1;
     // Start is called before the first frame update
     void Start()
@@ -16,7 +15,7 @@ public class PlayerController : MonoBehaviour
     }
     void Update()
     {
-        float x = Input.GetAxis("Horizontal" + playerNum) * speed; //Use Input.GetAxisRaw to get whole number values 0,1
+        float x = Input.GetAxis("Horizontal" + playerNum) * speed;
         float y = Input.GetAxis("Vertical" + playerNum) * speed;
         rb.velocity = new Vector2(x, y);
     }
