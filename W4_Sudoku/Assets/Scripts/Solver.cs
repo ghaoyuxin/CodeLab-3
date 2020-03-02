c64589b1c006791407542e2b5624cfb79b2fd464
ghaoyuxin <ghaoyuxin@gmail.com>
2020-03-01 23:05:32 -0500

    made a Solver script

---
+++ W4_Sudoku/Assets/Scripts/Solver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Given a text file representing an unsolved sudoku puzzle,
+//identify if the puzzle is
+//a) unsolvable,
+//b) has a unique solution,
+//or c) has multiple correct solutions.
+
+public class Solver : MonoBehaviour
+{
+    //make a grid
+    //if xx, return unsolvable
+    //if xx, return solved
+    //if xx, return manySolved
+    
+    
+
+}
